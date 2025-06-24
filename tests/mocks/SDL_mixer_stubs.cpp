54d8d3d8f103391084e433209acc7e2fa4a9ff0b
GordoxGit <romain.begot@gmail.com>
2025-06-24 17:24:40 +0200

    PE-228 Improve SDL_mixer stubs

--- tests/mocks/SDL_mixer_stubs.cpp
+++ tests/mocks/SDL_mixer_stubs.cpp
@@ -7,10 +7,15 @@ extern "C" int stub_last_halt_channel = -2;
 
 namespace {
 
+struct DummyChunk { int id; };
+struct DummyMusic { int id; };
+
 static int dummy_channels = 8;
-static int last_channel = 0;
-static int master_volume = MIX_MAX_VOLUME;
-static int music_volume = MIX_MAX_VOLUME;
+static int last_channel   = 0;
+static int master_volume  = MIX_MAX_VOLUME;
+static int music_volume   = MIX_MAX_VOLUME;
+static int next_chunk_id  = 0;
+static int next_music_id  = 0;
 
 extern "C" int Mix_OpenAudio(int, Uint16, int, int) { return 0; }
 extern "C" void Mix_CloseAudio() {}
@@ -20,21 +25,25 @@ extern "C" int Mix_AllocateChannels(int n) {
     return dummy_channels;
 }
 
-extern "C" Mix_Chunk* Mix_LoadWAV(const char*) {
-    static int fake_chunk;
-    return reinterpret_cast<Mix_Chunk*>(&fake_chunk);
+extern "C" Mix_Chunk* Mix_LoadWAV_RW(SDL_RWops*, int) {
+    return reinterpret_cast<Mix_Chunk*>(new DummyChunk{next_chunk_id++});
 }
 
+extern "C" Mix_Chunk* Mix_LoadWAV(const char*) { return Mix_LoadWAV_RW(nullptr, 0); }
+
 extern "C" Mix_Music* Mix_LoadMUS(const char*) {
-    static int fake_music;
-    return reinterpret_cast<Mix_Music*>(&fake_music);
+    return reinterpret_cast<Mix_Music*>(new DummyMusic{next_music_id++});
 }
 
 extern "C" int Mix_PlayChannel(int, Mix_Chunk*, int) { return last_channel++; }
 extern "C" int Mix_PlayMusic(Mix_Music*, int) { return 0; }
 extern "C" int Mix_FadeInMusic(Mix_Music*, int, int) { return 0; }
 
-extern "C" int Mix_HaltChannel(int c) { stub_last_halt_channel = c; return 0; }
+extern "C" int Mix_HaltChannel(int c) {
+    stub_last_halt_channel = c;
+    return 0;
+}
+
 extern "C" int Mix_HaltMusic() { return 0; }
 
 extern "C" void Mix_PauseMusic() {}
@@ -50,9 +59,14 @@ extern "C" int Mix_VolumeMusic(int v) {
     return music_volume;
 }
 
-extern "C" void Mix_FreeChunk(Mix_Chunk*) {}
-extern "C" void Mix_FreeMusic(Mix_Music*) {}
+extern "C" void Mix_FreeChunk(Mix_Chunk* c) {
+    delete reinterpret_cast<DummyChunk*>(c);
+}
 
-extern "C" const char* Mix_GetError() { return ""; }
+extern "C" void Mix_FreeMusic(Mix_Music* m) {
+    delete reinterpret_cast<DummyMusic*>(m);
+}
+
+extern "C" const char* Mix_GetError() { return "SDL_mixer stub"; }
 
 } // namespace
